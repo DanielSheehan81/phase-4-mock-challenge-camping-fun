9a6e291d03974c909c76dd0dd4fa6825d7aa26ee
DanielSheehan <daniel.sheehan81@yahoo.com>
2021-12-17 11:05:30 -0500

    Think I got it all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
+private
+
+    def record_not_found(exception)
+        render json: {error: "#{exception.model} not found"}, status: :not_found
+    end
+
+    def invalid_record(invalid)
+        render json: {error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+    end
 
 end
---
+++ app/controllers/campers_controller.rb
@@ -0,0 +1,27 @@
+class CampersController < ApplicationController
+    wrap_parameters format: []
+
+
+    def index
+        render json: Camper.all, status: :ok
+    end
+
+    def show
+        camper = Camper.find(params[:id])
+        render json: camper, status: :ok
+    end
+
+    def create
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
+
+
+
+end
